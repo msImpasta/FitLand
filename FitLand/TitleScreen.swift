5220ff3d11792c1f86823b88e198ca59a84b2469
Alexis Golland <alexisgolland@egolland.com>
2025-07-16 11:16:29 -0500

    k

--- FitLand/TitleScreen.swift
+++ FitLand/TitleScreen.swift
@@ -1,29 +1,8 @@
 import SwiftUI
 
 struct TitleScreen: View {
-<<<<<<< HEAD
-    var body: some View {
-        ZStack{
-            Color(red: 0.200, green: 0.70, blue: 0.75)
-                .edgesIgnoringSafeArea(.all).ignoresSafeArea()
-            Text("Welcome to FitLand!")
-                .font(.system(size: 30, weight: .bold, design: .default))
-                .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.521, saturation: 0.443, brightness: 0.995)/*@END_MENU_TOKEN@*/)
-                .padding()
-        }
-        HStack {
-            Image(systemName: "play.fill")
-                //.edgesIgnoringSafeArea(.all)
-            //.ignoresSafeArea()
-                    //.padding()
-                Text("Play!")
-            
-        }
-        
-        
-=======
-    @State private var startApp = false
->>>>>>> 32f212f86f6d0941a26d812849af27dd1612e245
+@State private var startApp = false
+
 
     var body: some View {
         if startApp {
